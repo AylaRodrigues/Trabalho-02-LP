fc2d189fcc37a48e5010a86b98166f68726a82ba
SofiaKitaeva <75208744+SofiaKitaeva@users.noreply.github.com>
2021-07-22 20:54:53 -0300

    Update Kila.h

--- Kila.h
+++ Kila.h
@@ -1,1 +1,29 @@
+#ifndef Kila_h
+#define Kila_h
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <locale.h>
+
+typedef struct Contador{
+	void* endereco;
+	int referencias;
+	struct Contador* prox;
+}contador;
+
+contador* SetKila();
+
+contador* InsereCont(void* x, contador *lista);
+
+void Atualiza(void* x, void* y, contador *lista);
+
+void dump(contador* lista);
+
+void dump(contador* lista);
+
+void* malloc2(size_t tam, contador* lista);
+
+void* atrib2(void* x, void* y, contador* lista);
+
+#endif
